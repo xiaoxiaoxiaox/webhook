ffa3a58072c9bb5c86b77d78bbde1f9fec5c8ed1
xixixi <1605882811@qq.com>
2019-03-31 22:33:15 +0800

    git 配置全局

--- blog.php
+++ blog.php
@@ -26,7 +26,7 @@ set_time_limit(0);
 exec('cd /home/www && ls -al && cat .gitconfig && git config --global -l 2>&1', $l);
 var_dump($l);
 
-exec("git config --global user.email '1602882811@qq.com' 2>&1&& git config --global user.email 'xiaoxiaoxiao' 2>&1&&  git config --global -l 2>&1", $test);
+exec("whoami 2>&1&&git config --global user.email '1602882811@qq.com' 2>&1&& git config --global user.email 'xiaoxiaoxiao' 2>&1&&  git config --global -l 2>&1", $test);
 var_dump($test);
 exit();
 
