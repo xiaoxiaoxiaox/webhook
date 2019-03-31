bfb99d5f3baf7b7e5aaabd6a8fc9d038eae15bfb
xixixi <1605882811@qq.com>
2019-03-31 23:08:38 +0800

    git 配置全局

--- blog.php
+++ blog.php
@@ -23,7 +23,7 @@ error_reporting(E_ALL);
 //设置请求超时时间
 set_time_limit(0);
 
-exec('cd /home/www && ls -al && cat .gitconfig && git config --global -l 2>&1', $l);
+exec('cd /home/www && ls -al && cat .gitconfig && git config --file /home/www/.gitconfig -l 2>&1', $l);
 var_dump($l);
 
 exec("whoami 2>&1&&git config --global user.email '1602882811@qq.com' 2>&1&& git config --global user.email 'xiaoxiaoxiao' 2>&1&&  git config --global -l 2>&1", $test);
