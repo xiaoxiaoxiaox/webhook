d9f5eb18d7a694eaf862a2aa32d61dc0d613e9d8
xixixi <1605882811@qq.com>
2019-03-31 11:03:53 +0800

    初始化钩子事件

---
+++ blog.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Url: http://xxcok.com
+ * Author: xiaoxiaoxiao
+ * Date: 2019/3/31
+ * Time: 9:48
+ *
+ *  .--,       .--,
+ * ( (  \.---./  ) )
+ *  '.__/o   o\__.'
+ *     {=  ^  =}
+ *      >  -  <
+ *     /       \
+ *    //       \\
+ *   //|   .   |\\
+ *   "'\       /'"_.-~^`'-.
+ *      \  _  /--'         `
+ *    ___)( )(___
+ *   (((__) (__)))    高山仰止,景行行止.虽不能至,心向往之。
+ * -----------------------------------------------------
+ */
+error_reporting(E_ALL);
+
+$gitPost = json_decode(file_get_contents("php://input"));
+
+$dir = '/home/www/blog';//该目录为git检出目录
+$comm = "cd {$dir}  && git checkout  master  && git pull origin master";
+$handle = popen($comm, 'r');
+$read = stream_get_contents($handle);
+echo "'$handle'; " . gettype($handle) . "\n";
+pclose($handle);
+echo json_encode($read);
